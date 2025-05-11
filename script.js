6016c9f40612004e5ce289c39b55089288660635
Mason Simmons <116126049+MilkManIsHere@users.noreply.github.com>
2025-05-11 16:46:49 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -11,28 +11,48 @@ var loseAudio = new Audio("lose.mp3")
 var noneAudio = new Audio("none.ogg")
 var balance = 2000
 var symbols = [
-	{ name: "brell", file: "1.png", frequency: 6 },
-	{ name: "brell-inverted", file: "2.png", frequency: 6 },
-	{ name: "color-inverted-brell", file: "3.png", frequency: 6 },
-	{ name: "color-inverted-brell-inverted", file: "4.png", frequency: 6 },
-	{ name: "omega-brell", file: "5.png", frequency: 5 },
-	{ name: "omega", file: "6.png", frequency: 5 },
-	{ name: "whoop", file: "7.png", frequency: 5 },
-	{ name: "mad-brell", file: "8.png", frequency: 5 },
-	{ name: "brell-foot", file: "9.png", frequency: 3 },
-	{ name: "brell-nado", file: "10.png", frequency: 1 }
+	{ name: "umbrella", file: "1.png", frequency: 7 },
+	{ name: "brellfoot", file: "2.png", frequency: 7 },
+	{ name: "brellnado", file: "3.png", frequency: 7 },
+	{ name: "ombrella", file: "4.png", frequency: 7 },
+	{ name: "omega", file: "5.png", frequency: 7 },
+	{ name: "omnado", file: "6.png", frequency: 6 },
+	{ name: "tacobrell", file: "7.png", frequency: 6 },
+	{ name: "limited-history", file: "8.png", frequency: 6 },
+	{ name: "madbrella", file: "9.png", frequency: 6 },
+	{ name: "analog-umbrella", file: "10.png", frequency: 6 },
+	{ name: "antibrella", file: "11.png", frequency: 5 },
+	{ name: "inverted-umbrella", file: "12.png", frequency: 5 },
+	{ name: "inverted-antibrella", file: "13.png", frequency: 5 },
+	{ name: "mason-umbrella", file: "14.png", frequency: 5 },
+	{ name: "oldbrella", file: "15.png", frequency: 5 },
+	{ name: "quadbrella", file: "16.png", frequency: 4 },
+	{ name: "mowbrell", file: "17.png", frequency: 4 },
+	{ name: "umbrella-twins", file: "18.png", frequency: 4 },
+	{ name: "brellcraft", file: "19.png", frequency: 3 },
+	{ name: "umbrella-signal", file: "20.png", frequency: 1 }
 ]
 var payouts = {
-	"brell": [0.5, 1],
-	"brell-inverted": [1, 1.5],
-	"color-inverted-brell": [1.5, 2],
-	"color-inverted-brell-inverted": [2, 2.5],
-	"omega-brell": [3, 3.5],
-	"omega": [3.5, 4],
-	"whoop": [4, 4.5],
-	"mad-brell": [4.5, 5],
-	"brell-foot": [6, 10],
-	"brell-nado": [20, 100]
+	"umbrella": [0.5, 1],
+	"brellfoot": [1, 1.5],
+	"brellnado": [1.5, 2],
+	"ombrella": [2, 2.5],
+	"omega": [2.5, 3],
+	"omnado": [3.5, 4],
+	"tacobrell": [4, 4.5],
+	"limited-history": [4.5, 5],
+	"madbrella": [5, 5.5],
+	"analog-umbrella": [5.5, 6],
+	"antibrella": [7, 7.5],
+	"inverted-umbrella": [7.5, 8],
+	"inverted-antibrella": [8, 8.5],
+	"mason-umbrella": [8.5, 9],
+	"oldbrella": [9, 9.5],
+	"quadbrella": [12, 14],
+	"mowbrell": [15, 17],
+	"umbrella-twins": [18, 20],
+	"brellcraft": [50, 100],
+	"umbrella-signal": [200, 1000]
 }
 var isSpinning = false
 function getSelectedBet() {
@@ -111,7 +131,7 @@ function spin() {
 			}
 		}
 		if (matched) balance += bet + payout
-		if (counts["brell-nado"] === 3) jackpotAudio.play()
+		if (counts["brell-signal"] === 3) jackpotAudio.play()
 		var net = matched ? payout : -bet
 		var message = ""
 		if (net > 0) {
